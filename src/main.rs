783c75f990e8d3d97ce1f411f27d8750db3cb0ec
kuttappi <iamkuttappiii@gmail.com>
2024-11-07 20:34:47 +0530

    solved some responsive problems

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ async fn main() -> std::io::Result<()>{
        .service(Files::new("/static", "./static"))
        .route("/", web::get().to(index))
     })
-    .bind("127.0.0.1:8083")?
+    .bind("127.0.0.1:8084")?
     .run()
     .await
 }
